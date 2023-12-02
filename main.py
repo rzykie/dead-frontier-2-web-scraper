835cd38a1e356cfeb59034d4e2f421150386f65e
Zykie Rallos <zykie1018@gmail.com>
2023-12-02 12:07:55 +0800

    Update: check for initial request is successful

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ url = "https://deadfrontier2.fandom.com/wiki/Skills"
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
+
+
+response = httpx.get(
+    url,
+    headers=headers,
+)
+
+print(response.text)
