2da026a94df58dddef92b4a6ec3118ea2ee135c9
Zykie Rallos <zykie1018@gmail.com>
2023-12-03 11:05:18 +0800

    Update: extract unique pistol names

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import httpx
 from selectolax.parser import HTMLParser
 
-url = "https://deadfrontier2.fandom.com/wiki/Skills"
+url = "https://deadfrontier2.fandom.com/wiki/Unique_Pistol"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
@@ -16,8 +16,16 @@ response = httpx.get(
 # response.text is the HTMl
 html = HTMLParser(response.text)
 
-skills = html.css("table.customtable td span")
+rows = html.css("table.customtable tbody tr")
 
-for skill in skills:
-    skill_progression = {"Skill": skill.text()}
-    print(skill_progression)
+pistol_names = []
+
+for row in rows:
+    # Extract pistol name if available
+    pistol_name_element = row.css("th span b")
+    pistol_name = pistol_name_element[0].text() if pistol_name_element else "N/A"
+
+    if pistol_name != "N/A":
+        pistol_names.append(pistol_name)
+
+print(pistol_names)
